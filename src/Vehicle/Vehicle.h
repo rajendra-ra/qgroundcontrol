27cb3f286b79d50b8906f5247bf0d1b3ae9cd13f
Rajendra Verma <rajendra.verma@range.aero>
2022-06-15 18:26:43 +0530

    Update Vehicle.h
    property declaration

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -303,6 +303,10 @@ public:
     Q_PROPERTY(Fact* hobbs              READ hobbs              CONSTANT)
     Q_PROPERTY(Fact* throttlePct        READ throttlePct        CONSTANT)
 
+//    Q_PROPERTY(Fact* fuelLevel          READ fuelLevel          CONSTANT)
+    Q_PROPERTY(Fact* engineRPM          READ engineRPM          CONSTANT)
+    Q_PROPERTY(Fact* rotorRPM           READ rotorRPM           CONSTANT)
+
     Q_PROPERTY(FactGroup*           gps             READ gpsFactGroup               CONSTANT)
     Q_PROPERTY(FactGroup*           gps2            READ gps2FactGroup              CONSTANT)
     Q_PROPERTY(FactGroup*           wind            READ windFactGroup              CONSTANT)
