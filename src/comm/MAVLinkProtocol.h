a27d9ed149421fb1b2935d573e13a05360bffd12
Rajendra Verma <rajendra.verma@range.aero>
2022-07-11 14:37:50 +0530

    ignore param signing

--- src/comm/MAVLinkProtocol.h
+++ src/comm/MAVLinkProtocol.h
@@ -68,7 +68,7 @@ public:
     static bool accept_unsigned_callback(const mavlink_status_t *status, uint32_t msgId)
     {
         Q_UNUSED(status);
-        return msgId == MAVLINK_MSG_ID_RADIO_STATUS || msgId == MAVLINK_MSG_ID_HEARTBEAT;
+        return msgId == MAVLINK_MSG_ID_RADIO_STATUS || msgId == MAVLINK_MSG_ID_HEARTBEAT || msgId == MAVLINK_MSG_ID_PARAM_VALUE;
     }
     /**
      * Reset the counters for all metadata for this link.
